960c23a05b477a3d7cf25751bdf6d44b579e6c99
Ito Yuki <32813641+madaraproject@users.noreply.github.com>
2022-02-03 15:02:52 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
 // グローバルメニュー関連
 $('.toggle-menu-btn, .header-navi-item').click(function(){
-    $('.toggle-menu-btn, .header-navi').toggleClass('closed');
-    $('body').toggleClass('noscroll');
+    $('.toggle-menu-btn, .header-navi, body').toggleClass('closed');
 });
 
 // リロード・リサイズ対応
 $(window).on('load resize', function(){
     if (window.matchMedia('(min-width: 1080px)').matches){
         $('.toggle-menu-btn, .header-navi').addClass('closed');
-        $('body').removeClass('noscroll');
+        $('body').removeClass('closed');
     }
 });
